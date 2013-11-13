e1589dc0d645cd3f13114d2844800a079e15178c
Tanner Smith <tanner@tssoftware.net>
2013-11-13 09:39:41 -0500

    Make fire a bit random in the animation times.

--- DaftMan/DMFire.m
+++ DaftMan/DMFire.m
@@ -10,13 +10,25 @@
 
 @implementation DMFire
 
+#define MAX_TIME_PER_FRAME 0.5
+#define MIN_TIME_PER_FRAME 0.2
+
+#define TOTAL_TIME 2.5
+#define TOTAL_TIME_VARIANCE 0.4
+
 - (id)initAtPosition:(CGPoint)point {
     SKTexture *fireLeft = [SKTexture textureWithImageNamed:@"fire-1.png"];
     SKTexture *fireRight = [SKTexture textureWithImageNamed:@"fire-2.png"];
     
     if (self = [super initWithTexture:fireLeft]) {
-        SKAction *flick = [SKAction animateWithTextures:@[fireLeft, fireRight] timePerFrame:0.3];
-        SKAction *flickering = [SKAction repeatAction:flick count:4];
+        NSTimeInterval timePerFrame = (arc4random() % (int) ((MAX_TIME_PER_FRAME - MIN_TIME_PER_FRAME) * 10 + 1) + (MIN_TIME_PER_FRAME * 10)) / 10.0;
+        
+        NSTimeInterval totalTimeVariance = (arc4random() % (int) (TOTAL_TIME_VARIANCE * 20 + 1)) / 10.0 - TOTAL_TIME_VARIANCE;
+                
+        int numberOfTimes = (TOTAL_TIME + totalTimeVariance) / (timePerFrame * 2);
+        
+        SKAction *flick = [SKAction animateWithTextures:@[fireLeft, fireRight] timePerFrame:timePerFrame];
+        SKAction *flickering = [SKAction repeatAction:flick count:numberOfTimes];
         
         SKAction *extinguish = [SKAction runBlock:^{
             [self removeFromParent];
